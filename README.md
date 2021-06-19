7ee0af824c72ce34ff62264a92efca19604e2890
Kgaugelo Molema <kmolema@gmail.com>
2021-06-19 10:26:48 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# wonderbank
+# Web API
+## .Net Core CLI
+* .NET Core SDK Version: 3.1.201
+
+## .Net Core librabries
+* Microsoft.EntityFrameworkCore
+* Swashbuckle.AspNetCore
+
+# Deploy locally
+* Install [.NET Core SDK](https://dotnet.microsoft.com/download/dotnet)
+
+* Bootstrap command `$TransactionsManagementSolutionDir\bootstrap.bat`
+
+Open https://localhost:5001/ to explore the API.
+
--- TransactionsManagement/Transactions/Startup.cs
+++ TransactionsManagement/Transactions/Startup.cs
@@ -73,19 +73,6 @@ namespace Transactions
             {
                 endpoints.MapControllers();
             });
-
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
-
-            //    spa.Options.SourcePath = "../ClientWebUI";
-
-            //    if (env.IsDevelopment())
-            //    {
-            //        spa.UseAngularCliServer(npmScript: "start");
-            //    }
-            //});
         }
     }
 }
